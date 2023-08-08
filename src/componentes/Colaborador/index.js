56f3e68db168421ce0c16807239b75d3121eaa12
Sofia <sofibareta@gmail.com>
2023-08-08 13:20:30 -0300

    react components

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,26 @@
+// não é um export default
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    const favoritar = () => aoFavoritar(colaborador.id)
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className="colaborador">
+        <AiFillCloseCircle size={25} className="deletar" onClick={() => aoDeletar(colaborador.id)} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito ? <AiFillHeart {...propsFavorito} color='#FF0000' /> : <AiOutlineHeart {...propsFavorito}/>}
+            </div>
         </div>
     </div>)
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,13 +1,14 @@
+import hexToRgba from 'hex-to-rgba'
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input type='color' className='input-cor' value={time.cor} onChange={e => mudarCor(e.target.value, time.id)}/>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} aoFavoritar={aoFavoritar}/>)}
             </div>
         </section>
 
